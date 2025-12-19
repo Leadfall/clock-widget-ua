416e5e32403a6ed52d5a832817cabb54a8b67603
Andrew <47742913+Leadfall@users.noreply.github.com>
2025-12-19 16:23:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-////////////////////
-// URL PARAMETERS //
-////////////////////
+// URL PARAMETERS
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const dateFormat = urlParams.get("dateFormat") || 'ddd DD MMM yyyy hh:mm:ss A';
+const dateFormat = urlParams.get("dateFormat") || 'ddd DD MMM yyyy HH:mm:ss';
 
-///////////////
-// FUNCTIONS //
-///////////////
+const locale = urlParams.get('locale');
+if (locale === 'en') {
+    moment.locale('en');
+} else {
+    moment.locale('uk');
+}
+
+// FUNCTIONS
 
 function setTime()
 {
     document.getElementById("timeLabel").innerHTML = moment().format(dateFormat);
-    setTimeout(setTime, 1000);
 }
 
 setTime();
+setInterval(setTime, 1000);
